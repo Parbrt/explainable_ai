b78f337260fc38277279e3ecc595ecd086719bed
Paul-Aimé <paulaime.robert@lilo.org>
2026-04-17 15:04:49 +0200

    feat: xgb

--- src/XGBoost_impl/XGBoost_impl.py
+++ src/XGBoost_impl/XGBoost_impl.py
@@ -4,11 +4,13 @@
     HERBRETEAU Mathis, LE POTTIER Mathias, ROBERT Paul-Aimé
 """
 
+import shap
 import xgboost as xgb
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import classification_report
 import numpy as np
 from src.XGBoost_impl.SpecificDataSet import SpecificDataSet
+import matplotlib.pyplot as plt
 
 
 class XGBoostImpl():
@@ -73,7 +75,7 @@ class XGBoostImpl():
         return self.model.predict(input_scaled)
 
 
-    def test_random_prediction(self):
+    def test_random_prediction(self, do_explain = False):
         """
         Sélectionne une ligne aléatoire dans le set de test, 
         prédit les cibles et affiche la comparaison avec le réel.
@@ -100,9 +102,44 @@ class XGBoostImpl():
         print("columns :\n",sample_x)
         print("val réel : ",true_y)
         print("val prédite : ",pred_y)
-            
-            
+
+        if do_explain:
+            self.explain_line(random_index)
+
+    def explain_line(self,line = 3):
+
+        if self.model is None:
+            print("Erreur : Le modèle doit être entraîné avant le test.")
+            return
+        # Create a tree explainer
+        xgb_explainer = shap.TreeExplainer(self.model)
+
+
+        shap_explainer_values = xgb_explainer(self.X_test)
+        shap.waterfall_plot(shap_explainer_values[line])
+
+        plt.show()
+
+    def explain_global(self):
+
+        if self.model is None:
+            print("Erreur : Le modèle doit être entraîné avant le test.")
+            return
+
+
+        explainer = shap.TreeExplainer(self.model)
+        shap_values = explainer(self.X_test)
+
+        # Graphique global complet
+        shap.summary_plot(shap_values, self.X_test)
+
+        plt.show()
 if __name__ == "__main__":
     model = XGBoostImpl()
     model.train()
-    model.test_random_prediction()
+    model.explain_global()
+    input_word = 0
+    while input_word != 1:
+        input_word = input("press 1 to exit\n>>")
+        model.test_random_prediction(do_explain=True)
+
