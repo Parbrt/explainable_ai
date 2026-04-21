261deb9a4c3b461662a06d00fb57928b055ec629
Mathis Herbreteau <mathis.herbreteau55@orange.fr>
2026-04-21 13:57:16 +0200

    AUG LogReg

--- src/LogisticReg_impl/LogisticReg_impl.py
+++ src/LogisticReg_impl/LogisticReg_impl.py
@@ -5,6 +5,8 @@ from src.utils.data_prep import DataSet
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, roc_auc_score
+
 
 class SpecificDataSet(DataSet):
     def specific_prep(self, test_size=0.2, one_hot_enc=True):
@@ -14,7 +16,7 @@ class SpecificDataSet(DataSet):
         return scaler.fit_transform(X_train), scaler.transform(X_test), y_train, y_test
 
 def train_logistic_regression(X_train, y_train):
-    model = LogisticRegression(random_state=42, max_iter=1000)
+    model = LogisticRegression(random_state=42, max_iter=100)
     return model.fit(X_train, y_train)
 
 def get_shap_values(model, X_train, X_test):
@@ -40,6 +42,9 @@ if __name__ == "__main__":
     
     model = train_logistic_regression(X_train, y_train)
     print("--- Rapport de Classification ---\n", classification_report(y_test, model.predict(X_test)))
+    
+    y_proba = model.predict_proba(X_test)[:, 1]
+    print("AUC :", roc_auc_score(y_test, y_proba))
     
     shap_vals, expected_val = get_shap_values(model, X_train, X_test)
     
